ea65cb8f6d5c84acb589261010ecb518370b49c1
Paul Dangelo <pdangelo1988@gmail.com>
2023-05-01 20:25:56 -0400

    .

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,8 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/booksearch', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/stormy-cove-50414', {
+
 });
 
 module.exports = mongoose.connection;
